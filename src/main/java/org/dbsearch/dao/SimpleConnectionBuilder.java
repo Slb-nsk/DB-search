891bca542bd8b1e52ab1ffd579c0bf805341649e
Slb-nsk <wusong@ngs.ru>
2020-09-19 23:10:32 +0700

    database connection through ini-file

--- src/main/java/org/dbsearch/dao/SimpleConnectionBuilder.java
+++ src/main/java/org/dbsearch/dao/SimpleConnectionBuilder.java
@@ -1,13 +1,15 @@
 package org.dbsearch.dao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class SimpleConnectionBuilder implements ConnectionBuilder {
-    static final String DB_URL = "jdbc:postgresql://localhost:5432/base1";
-    static final String USERNAME = "postgres";
-    static final String PASSWORD = "";
+    static final String DATA_FILE = "program.ini";
     static final String DRIVER = "org.postgresql.Driver";
 
     public SimpleConnectionBuilder() {
@@ -15,14 +17,43 @@ public class SimpleConnectionBuilder implements ConnectionBuilder {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            System.exit(1);
         }
     }
 
     @Override
     public Connection getConnection() throws SQLException {
-        String url = DB_URL;
-        String username = USERNAME;
-        String password = PASSWORD;
+
+        String url = "";
+        String username = "";
+        String password = "";
+
+        //считываем параметры для соединения с базой данных
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(DATA_FILE));
+            ArrayList<String> parameters = new ArrayList<>();
+            String s;
+            while ((s = br.readLine()) != null) {
+                parameters.add(s);
+            }
+            for (String stroka : parameters) {
+                String[] words = stroka.split("=");
+                switch (words[0].trim()) {
+                    case "DB_URL":
+                        url = "jdbc:postgresql://" + words[1].trim().substring(1,words[1].length()-2);
+                        break;
+                    case "USERNAME":
+                        username = words[1].trim().substring(1,words[1].length()-2);
+                        break;
+                    case "PASSWORD":
+                        password = words[1].trim().substring(1,words[1].length()-2);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
         return DriverManager.getConnection(url, username, password);
     }
 }
